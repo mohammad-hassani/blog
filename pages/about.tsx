dc618ac31a72cdf477545159982d77b5a87aa82e
mohammad hassani <59317946+mohammad-hassani@users.noreply.github.com>
2023-08-01 20:35:32 +0330

    fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,9 +13,9 @@ const About: NextPage = () => {
             Welcome to My Blog This is where I share my thoughts, ideas, and experiences on various topics. You can find articles, tutorials, and more on subjects like technology, science, lifestyle, and personal development
           </p>
           <p>
-            My name is Mohammad, I'm 23 and a computer science student based in Rafsanjan. I have a strong passion for the technologies related to the Front-end web development
-            Outside of my regular activities, I'm into designing, making content in my channel, develoaping npm packages, SEO and learning new stuff whenever I have some free time. .
-            I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !
+            My name is Mohammad, I&apos;m 23 and a computer science student based in Rafsanjan. I have a strong passion for the technologies related to the Front-end web development
+            Outside of my regular activities, I&apos;m into designing, making content in my channel, develoaping npm packages, SEO and learning new stuff whenever I have some free time. .
+            I&apos;m continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !
           </p>
           <p>
             Here, you will find a collection of articles and blog posts written by me on a regular basis. I aim to publish new content every week, so make sure to check back regularly for updates.
